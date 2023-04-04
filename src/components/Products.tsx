6d22ade977ba2a15484af6f8f4c4a5fecbb63dac
david zambrano corral <Yayoamigo>
2023-04-04 11:08:01 -0500

    loadbalancer not https so move to ec2 for requests https

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -44,7 +44,7 @@ const [filteredProducts, setFilteredProducts] = useState<ProductData[]>([]);
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get(cat ? `http://zucaLB-1301733182.us-east-1.elb.amazonaws.com/api/products?category=${cat}` : "http://zucaLB-1301733182.us-east-1.elb.amazonaws.com/api/products");
+        const res = await axios.get(cat ? `https://52.205.251.111:5000/api/products?category=${cat}` : "https://52.205.251.111:5000/api/products");
         setProducts(res.data);
       } catch (err) {
 
